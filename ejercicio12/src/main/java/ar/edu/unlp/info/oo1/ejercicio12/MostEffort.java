e26c3fa09b9a8a3af429e3728d28835f1f975a40
Valentina <129221307+valpenna@users.noreply.github.com>
2025-10-09 15:17:56 -0300

    Update MostEffort.java

--- ejercicio12/src/main/java/ar/edu/unlp/info/oo1/ejercicio12/MostEffort.java
+++ ejercicio12/src/main/java/ar/edu/unlp/info/oo1/ejercicio12/MostEffort.java
@@ -8,16 +8,11 @@ public class MostEffort extends JobScheduler {
 		this.strategy = "MostEffort";
 	}
 
-	@Override
-	public JobDescription next() {
-        JobDescription nextJob = null;
-		
-		nextJob = jobs.stream()
-                .max((j1,j2) -> Double.compare(j1.getEffort(), j2.getEffort()))
-                .orElse(null);
-            this.unschedule(nextJob);
-            return nextJob;
+	public JobDescription nextJob() {
+            return jobs.stream()
+                    .max((j1,j2) -> Double.compare(j1.getEffort(), j2.getEffort()))
+                    .orElse(null);
 	}
 	
-	
 }
+
