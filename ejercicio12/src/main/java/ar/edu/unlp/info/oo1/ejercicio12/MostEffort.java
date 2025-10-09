62966cfe2fec6b69e6cd2958455de7e7c7756fb9
Valentina <129221307+valpenna@users.noreply.github.com>
2025-10-09 15:19:53 -0300

    correcion MostEffort.java

--- ejercicio12/src/main/java/ar/edu/unlp/info/oo1/ejercicio12/MostEffort.java
+++ ejercicio12/src/main/java/ar/edu/unlp/info/oo1/ejercicio12/MostEffort.java
@@ -8,11 +8,12 @@ public class MostEffort extends JobScheduler {
 		this.strategy = "MostEffort";
 	}
 
-	public JobDescription nextJob() {
+	protected JobDescription nextJob() {
             return jobs.stream()
                     .max((j1,j2) -> Double.compare(j1.getEffort(), j2.getEffort()))
                     .orElse(null);
 	}
 	
 }
+
 
