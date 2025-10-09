0b0108ea93c298ec91af5021c02b166d4b691f8d
Valentina <129221307+valpenna@users.noreply.github.com>
2025-10-09 15:19:01 -0300

    CORRECCION HighestPriority.java

--- ejercicio12/src/main/java/ar/edu/unlp/info/oo1/ejercicio12/HighestPriority.java
+++ ejercicio12/src/main/java/ar/edu/unlp/info/oo1/ejercicio12/HighestPriority.java
@@ -9,14 +9,11 @@ public class HighestPriority extends JobScheduler {
 	}
 
 	
-	public JobDescription next() {
-        JobDescription nextJob = null;
-
-		nextJob = jobs.stream()
+	protected JobDescription nextJob() {
+		return jobs.stream()
                 .max((j1,j2) -> Double.compare(j1.getPriority(), j2.getPriority()))
                 .orElse(null);
-            this.unschedule(nextJob);
-            return nextJob;
-	}
-	
+	};
+		
 }
+
