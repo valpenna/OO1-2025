87941904081b99cce5b89ae1a4646cb8ba2a5266
Valentina <129221307+valpenna@users.noreply.github.com>
2025-10-30 15:23:21 -0300

    correcion reserva

--- ejercicio19/src/main/java/ar/edu/unlp/info/oo1/ejercicio19/Reserva.java
+++ ejercicio19/src/main/java/ar/edu/unlp/info/oo1/ejercicio19/Reserva.java
@@ -1,5 +1,7 @@
 package ar.edu.unlp.info.oo1.ejercicio19;
 
+import java.time.LocalDate;
+
 public class Reserva {
 	private DateLapse periodo;
 	private Propiedad propiedad;
@@ -13,7 +15,15 @@ public class Reserva {
 		return this.periodo;
 	}
 	
+	public boolean coincide(DateLapse otroPeriodo) {
+		return getPeriodo().overlaps(otroPeriodo);
+	}
+	
+	public boolean incluyeFechaHoy() {
+		return this.periodo.includesDate(LocalDate.now());
+	}
+	
 	public double calcularPrecioReserva() {
-		return this.propiedad.getPrecioPorNoche() * periodo.sizeInDays();
+		return this.propiedad.devolverPrecioPorPeriodo(this.periodo);
 	}
 }
