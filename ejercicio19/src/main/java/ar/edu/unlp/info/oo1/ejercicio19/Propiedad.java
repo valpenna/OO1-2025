3523d3d9a316d5022a5f4e08b61b05388bd40a20
Valentina <129221307+valpenna@users.noreply.github.com>
2025-11-05 16:27:00 -0300

    valores borde particiones equivalentes
    Refactor Propiedad class to improve reservation handling

--- ejercicio19/src/main/java/ar/edu/unlp/info/oo1/ejercicio19/Propiedad.java
+++ ejercicio19/src/main/java/ar/edu/unlp/info/oo1/ejercicio19/Propiedad.java
@@ -11,7 +11,7 @@ public class Propiedad {
 	private List<Reserva> reservas;
 	
 	
-	public Propiedad(String direccion, String nombre, double precioPorNoche, Usuario propietario) {
+	public Propiedad(String direccion, String nombre, double precioPorNoche) {
 		this.direccion = direccion;
 		this.nombreDescriptivo = nombre;
 		this.precioPorNoche = precioPorNoche;
@@ -34,22 +34,22 @@ public class Propiedad {
 		return esta; //verificar que sea true
 	}
 	
-	public void crearReserva(LocalDate fechaInicio, LocalDate fechaFin) {
+	public Reserva crearReserva(LocalDate fechaInicio, LocalDate fechaFin) {
 		if(this.estaDisponible(fechaInicio, fechaFin)) {
 			DateLapse periodo = new DateLapse(fechaInicio, fechaFin);
 			Reserva reserva = new Reserva(this, periodo);
 			this.reservas.add(reserva);
+			return reserva;
 		}
+		return null;
+		
 	}
 	
 	
 	public boolean cancelarReserva(Reserva reserva) {
-		if(reserva.incluyeFechaHoy()) {
-			return false;
-		} else {
-			this.reservas.remove(reserva);
-			return true;
-		}
+		if (!this.reservas.contains(reserva)) return false;
+	    if (reserva.incluyeFechaHoy()) return false;
+	    return this.reservas.remove(reserva);
 	}
 	
 	public double devolverPrecioPorPeriodo(DateLapse periodo) {
@@ -67,3 +67,37 @@ public class Propiedad {
 }
 
 
+/*	Disponibilidad de una propiedad en una fecha, reservar y cancelar reserva
+ 	1. Para la disponibilidad de una reserva se testea el metodo estaDisponible() el cual devuelve un booleano
+ 	 	PARTICIONES EQUIVALENTES
+ 	Siendo que estan las particiones en donde:
+ 	.Espero que el resultado sea true si -> no hay ninguna reserva /el periodoi solicitado de la reserva no coincide con otra,
+ 	.Espero que el resultado sea false si -> el periodo solicitado comienza dentro de una reserva que existe 
+ 											el periodo solicitado termina dentro de una reserva que existe 
+ 											el periodo se superpone totalmente con una reserva que existe
+ 										 	una reserva existente está adentro del periodo solicitado
+ 		VALORES BORDE 
+ 	Se encuentran en el dia que comienza y termina una reserva
+ 	.en donde un borde al comienzo es el dia anterior y el dia que comienza(incluido en la reserva).
+	.en donde otro borde al final es el dia que termina(incluido en la reserva) y el dia siguiente.
+	
+	 	
+	2. Para cancelar una reserva se testea el metodo cancelarReserva()  el cual devuelve un booleano
+		PARTICIONES EQUIVALENTES
+	Siendo que estan las particiones en donde:
+	.Espero que el resultado sea true si -> la reserva no esta en curso / la cancelacion se realiza a mas de dos dias de iniciar la reserva
+	.Espero que el resultado sea false si -> la reserva esta en curso / al momento de cancelar la reserva faltan menos de dos dias para que inicie
+		VALORES BORDE
+	Se encuentran en el limite entre dos dias antes de la reserva y dia anterior
+	ej: la reserva es del 5 al 10, y se quiere cancelar el 3(da false) 
+	
+
+	3. Para agregar una reserva se testea el metodo agregarReserva() el cual devuelve un booleano
+		PARTICIONES EQUIVALENTES
+	Siendo que estan las particiones donde
+	.Espero que el resultado sea true si -> el metodo estaDisponible devuelve true
+	.Espero que el resultado sea false si -> el metodo estaDisponible devuelve false
+	
+		VALORES BORDE
+	Idem estaDisponible.
+ * */
